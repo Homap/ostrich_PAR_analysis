a9234ed78ff3d5a24c9b0da388b79e624e87d084
Homa Papoli Yazdi <hpapoli@gmail.com>
2022-06-16 11:53:27 +0200

    reduced number of snps per window from 2000 to 1000

--- code/analysis/rho/1_run_vcf_to_ldhat_input.sh
+++ code/analysis/rho/1_run_vcf_to_ldhat_input.sh
@@ -9,29 +9,36 @@ mkdir -p ../../../data/rho/ldhat_input/chr4 \
 # For autosomes, we are only interested to have a mean of rho to compare with the PAR. To reduce the computation time, only the longest scaffolds from each
 # chromosome 4 and 5 is chosen for the rho analysis.
 
+snp_size=1000
+snp_overlap=200
+num_win=10000
+
 echo "----------------------chr4----------------------"
 export chr4_dir=../../../data/vcf/ld_vcf/a_vcf/chr4
-python vcf_to_ldhat_out.py ${chr4_dir}/superscaffold11.chr4.vcf.gz ../../../data/lastz/gg_chr4_ostrich.bed 2000 200 superscaffold11 10000 ../../../data/rho/ldhat_input/chr4
+python vcf_to_ldhat_out.py ${chr4_dir}/superscaffold11.chr4.vcf.gz ../../../data/lastz/gg_chr4_ostrich.bed $snp_size $snp_overlap superscaffold11 $num_win ../../../data/rho/ldhat_input/chr4
 
 echo "----------------------chr5----------------------"
 export chr5_dir=../../../data/vcf/ld_vcf/a_vcf/chr5
-python vcf_to_ldhat_out.py ${chr5_dir}/superscaffold8.chr5.vcf.gz ../../../data/lastz/gg_chr5_ostrich.bed 2000 200 superscaffold8 10000 ../../../data/rho/ldhat_input/chr5
+python vcf_to_ldhat_out.py ${chr5_dir}/superscaffold8.chr5.vcf.gz ../../../data/lastz/gg_chr5_ostrich.bed $snp_size $snp_overlap superscaffold8 $num_win ../../../data/rho/ldhat_input/chr5
 
 echo "----------------------chrZ--PAR-----------------"
 export z_dir=../../../data/vcf/z_vcf
 for scaffold in $(cat ../../../data/bed/par_scaf.bed | grep -v "^chrom" | cut -f1 | sort | uniq) 
 do
     echo $scaffold
-    python vcf_to_ldhat_out.py ${z_dir}/z_vcf.gz ../../../data/bed/par_scaf.bed 2000 200 $scaffold 10000 ../../../data/rho/ldhat_input/z/par
+    python vcf_to_ldhat_out.py ${z_dir}/z_vcf.gz ../../../data/bed/par_scaf.bed $snp_size $snp_overlap $scaffold $num_win ../../../data/rho/ldhat_input/z/par
 done
 echo "----------------------chrZ--nonPAR--------------"
 echo "Produce a male - VCF for nonPAR"
 
-module load bioinfo-tools vcftools/0.1.16 htslib/1.14
-
-export nonpar_vcf=../../../data/vcf/nonpar_vcf/nonpar_vcf.filtered.repeatmasked.nofemalehet.hwe.vcf.gz
-vcftools --gzvcf ${nonpar_vcf} --keep ../../../data/samples/black_male.txt --recode --recode-INFO-all --stdout | bgzip -c > \
-../../../data/vcf/nonpar_vcf/male_nonpar_vcf.filtered.repeatmasked.nofemalehet.hwe.vcf.gz
+if [[ ! -f ../../../data/vcf/nonpar_vcf/male_nonpar_vcf.filtered.repeatmasked.nofemalehet.hwe.vcf.gz ]]
+then
+    echo "Loading modules"
+    module load bioinfo-tools vcftools/0.1.16 htslib/1.14
+    export nonpar_vcf=../../../data/vcf/nonpar_vcf/nonpar_vcf.filtered.repeatmasked.nofemalehet.hwe.vcf.gz
+    vcftools --gzvcf ${nonpar_vcf} --keep ../../../data/samples/black_male.txt --recode --recode-INFO-all --stdout | bgzip -c > \
+    ../../../data/vcf/nonpar_vcf/male_nonpar_vcf.filtered.repeatmasked.nofemalehet.hwe.vcf.gz
+fi
 
 echo "Produce nonPAR windows for male nonPAR vcf"
 export male_nonPAR=../../../data/vcf/nonpar_vcf/male_nonpar_vcf.filtered.repeatmasked.nofemalehet.hwe.vcf.gz
@@ -39,5 +46,5 @@ export male_nonPAR=../../../data/vcf/nonpar_vcf/male_nonpar_vcf.filtered.repeatm
 for scaffold in $(cat ../../../data/bed/nonpar_scaf.bed | grep -v "^chrom" | cut -f1 | sort | uniq) 
 do
     echo $scaffold
-    python vcf_to_ldhat_out.py ${male_nonPAR} ../../../data/bed/nonpar_scaf.bed 2000 200 $scaffold 10000 ../../../data/rho/ldhat_input/z/nonpar
+    python vcf_to_ldhat_out.py ${male_nonPAR} ../../../data/bed/nonpar_scaf.bed $snp_size $snp_overlap $scaffold $num_win ../../../data/rho/ldhat_input/z/nonpar
 done
