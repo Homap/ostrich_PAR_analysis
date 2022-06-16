0a6e1461ce40c1031a9fe0ccb619db9f66bfe654
Homa Papoli Yazdi <hpapoli@gmail.com>
2022-06-16 16:40:58 +0200

    removing psmc

--- code/analysis/rho/1_run_vcf_to_ldhat_input.sh
+++ code/analysis/rho/1_run_vcf_to_ldhat_input.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+module load bcftools/1.14 htslib/1.14 vcftools/0.1.16
+
 echo "Make output directories"
 mkdir -p ../../../data/rho/ldhat_input/chr4 \
          ../../../data/rho/ldhat_input/chr5 \
@@ -15,18 +17,20 @@ num_win=10000
 
 echo "----------------------chr4----------------------"
 export chr4_dir=../../../data/vcf/ld_vcf/a_vcf/chr4
-python vcf_to_ldhat_out.py ${chr4_dir}/superscaffold11.chr4.vcf.gz ../../../data/lastz/gg_chr4_ostrich.bed $snp_size $snp_overlap superscaffold11 $num_win ../../../data/rho/ldhat_input/chr4
+bcftools sort ${chr4_dir}/superscaffold11.chr4.vcf.gz | bgzip -c > ${chr4_dir}/superscaffold11.chr4.sorted.vcf.gz
+python vcf_to_ldhat_out.py ${chr4_dir}/superscaffold11.chr4.sorted.vcf.gz ../../../data/lastz/gg_chr4_ostrich.bed $snp_size $snp_overlap superscaffold11 $num_win ../../../data/rho/ldhat_input/chr4
 
 echo "----------------------chr5----------------------"
 export chr5_dir=../../../data/vcf/ld_vcf/a_vcf/chr5
-python vcf_to_ldhat_out.py ${chr5_dir}/superscaffold8.chr5.vcf.gz ../../../data/lastz/gg_chr5_ostrich.bed $snp_size $snp_overlap superscaffold8 $num_win ../../../data/rho/ldhat_input/chr5
+bcftools sort ${chr5_dir}/superscaffold8.chr5.vcf.gz | bgzip -c > ${chr5_dir}/superscaffold8.chr5.sorted.vcf.gz
+python vcf_to_ldhat_out.py ${chr5_dir}/superscaffold8.chr5.sorted.vcf.gz ../../../data/lastz/gg_chr5_ostrich.bed $snp_size $snp_overlap superscaffold8 $num_win ../../../data/rho/ldhat_input/chr5
 
 echo "----------------------chrZ--PAR-----------------"
 export z_dir=../../../data/vcf/z_vcf
 for scaffold in $(cat ../../../data/bed/par_scaf.bed | grep -v "^chrom" | cut -f1 | sort | uniq) 
 do
     echo $scaffold
-    python vcf_to_ldhat_out.py ${z_dir}/z_vcf.gz ../../../data/bed/par_scaf.bed $snp_size $snp_overlap $scaffold $num_win ../../../data/rho/ldhat_input/z/par
+    python vcf_to_ldhat_out.py ${z_dir}/sorted.z.vcf.gz ../../../data/bed/par_scaf.bed $snp_size $snp_overlap $scaffold $num_win ../../../data/rho/ldhat_input/z/par
 done
 echo "----------------------chrZ--nonPAR--------------"
 echo "Produce a male - VCF for nonPAR"
@@ -34,7 +38,6 @@ echo "Produce a male - VCF for nonPAR"
 if [[ ! -f ../../../data/vcf/nonpar_vcf/male_nonpar_vcf.filtered.repeatmasked.nofemalehet.hwe.vcf.gz ]]
 then
     echo "Loading modules"
-    module load bioinfo-tools vcftools/0.1.16 htslib/1.14
     export nonpar_vcf=../../../data/vcf/nonpar_vcf/nonpar_vcf.filtered.repeatmasked.nofemalehet.hwe.vcf.gz
     vcftools --gzvcf ${nonpar_vcf} --keep ../../../data/samples/black_male.txt --recode --recode-INFO-all --stdout | bgzip -c > \
     ../../../data/vcf/nonpar_vcf/male_nonpar_vcf.filtered.repeatmasked.nofemalehet.hwe.vcf.gz
@@ -42,9 +45,13 @@ fi
 
 echo "Produce nonPAR windows for male nonPAR vcf"
 export male_nonPAR=../../../data/vcf/nonpar_vcf/male_nonpar_vcf.filtered.repeatmasked.nofemalehet.hwe.vcf.gz
+bcftools sort ${male_nonPAR} | bgzip -c > ../../../data/vcf/nonpar_vcf/male_nonpar_vcf.filtered.repeatmasked.nofemalehet.hwe.sorted.vcf.gz
 
 for scaffold in $(cat ../../../data/bed/nonpar_scaf.bed | grep -v "^chrom" | cut -f1 | sort | uniq) 
 do
     echo $scaffold
-    python vcf_to_ldhat_out.py ${male_nonPAR} ../../../data/bed/nonpar_scaf.bed $snp_size $snp_overlap $scaffold $num_win ../../../data/rho/ldhat_input/z/nonpar
+    python vcf_to_ldhat_out.py ../../../data/vcf/nonpar_vcf/male_nonpar_vcf.filtered.repeatmasked.nofemalehet.hwe.sorted.vcf.gz \
+     ../../../data/bed/nonpar_scaf.bed $snp_size $snp_overlap $scaffold $num_win ../../../data/rho/ldhat_input/z/nonpar
 done
+
+
--- code/analysis/rho/ldhat_interval.sh
+++ code/analysis/rho/ldhat_interval.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -A snic2022-22-149 
 #SBATCH -p core
-#SBATCH -n 2
+#SBATCH -n 1
 #SBATCH -t 03:00:00
 #SBATCH --mail-user=homa.papoli_yazdi@biol.lu.se
 #SBATCH --mail-type=FAIL
@@ -12,4 +12,4 @@ locs=$2
 lk=$3
 out_prefix=$4
 
-./LDhat/interval -seq $sites -loc $locs -lk $lk -prefix $out_prefix -its 30000000 -bpen 5 -samp 6000
+./LDhat/interval -seq $sites -loc $locs -lk $lk -prefix $out_prefix -its 25000000 -bpen 5 -samp 5000
