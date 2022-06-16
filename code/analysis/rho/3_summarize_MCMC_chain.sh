91083846cb0669acbb6759eb0fc34bf8296dfe69
Homa Papoli Yazdi <hpapoli@gmail.com>
2022-06-16 23:12:47 +0200

    final mcmc summarizing script

--- code/analysis/rho/3_summarize_MCMC_chain.sh
+++ code/analysis/rho/3_summarize_MCMC_chain.sh
@@ -1,10 +1,36 @@
 #!/usr/bin/bash
 
-## Create the Tracer input to check for the distribution of likelihood, block number and map length
+# -----------------------------------------------------------------------------------------------------------
+# Create MCMC summary output directories
+# -----------------------------------------------------------------------------------------------------------
+
+mkdir -p ../../../data/rho/ldhat_mcmc/chr4 \
+         ../../../data/rho/ldhat_mcmc/chr5 \
+         ../../../data/rho/ldhat_mcmc/z/par \
+         ../../../data/rho/ldhat_mcmc/z/nonpar 
+
+# -----------------------------------------------------------------------------------------------------------
+# Set path to ldhat input directories:
+# -----------------------------------------------------------------------------------------------------------
+
+export mcmc_chr4=../../../data/rho/ldhat_mcmc/chr4
+export mcmc_chr5=../../../data/rho/ldhat_mcmc/chr5
+export mcmc_par=../../../data/rho/ldhat_mcmc/z/par
+export mcmc_nonpar=../../../data/rho/ldhat_mcmc/z/nonpar
+
+# -----------------------------------------------------------------------------------------------------------
+# Set path to ldhat input and output directories
+# -----------------------------------------------------------------------------------------------------------
+
 export ldhat_output_chr4=../../../data/rho/ldhat_output/chr4
 export ldhat_output_chr5=../../../data/rho/ldhat_output/chr5
 export ldhat_output_par=../../../data/rho/ldhat_output/z/par
 export ldhat_output_nonpar=../../../data/rho/ldhat_output/z/nonpar
+
+export chr4_dir=../../../data/rho/ldhat_input/chr4
+export chr5_dir=../../../data/rho/ldhat_input/chr5
+export par_dir=../../../data/rho/ldhat_input/z/par
+export nonpar_dir=../../../data/rho/ldhat_input/z/nonpar
 
 # -----------------------------------------------------------------------------------------------------------
 # - Autosome
@@ -20,7 +46,8 @@ do
     win_num=$(echo $window | cut -f8 -d "/" | cut -f4 -d ".")
     echo $win_num
     grep 'LK' ${ldhat_output_chr4}/${scaffold}.${win_num}.${mcmc} | \
-    awk 'BEGIN{print "iterations""\t""likelihood""\t""blocks""\t""map_length"} {print NR"\t"$5"\t"$10"\t"$15}' > ${ldhat_output_chr4}/${scaffold}.${win_num}.${mcmc}.chainreport.txt
+    awk 'BEGIN{print "iterations""\t""likelihood""\t""blocks""\t""map_length"} {print NR"\t"$5"\t"$10"\t"$15}' \
+    > ${mcmc_chr4}/${scaffold}.${win_num}.${mcmc}.chainreport.txt
 done
 done
 
@@ -34,7 +61,8 @@ do
     win_num=$(echo $window | cut -f8 -d "/" | cut -f4 -d ".")
     echo $win_num
     grep 'LK' ${ldhat_output_chr5}/${scaffold}.${win_num}.${mcmc} | \
-    awk 'BEGIN{print "iterations""\t""likelihood""\t""blocks""\t""map_length"} {print NR"\t"$5"\t"$10"\t"$15}' > ${ldhat_output_chr5}/${scaffold}.${win_num}.${mcmc}.chainreport.txt
+    awk 'BEGIN{print "iterations""\t""likelihood""\t""blocks""\t""map_length"} {print NR"\t"$5"\t"$10"\t"$15}' \
+    > ${mcmc_chr5}/${scaffold}.${win_num}.${mcmc}.chainreport.txt
 done
 done
 
@@ -53,7 +81,8 @@ do
         win_num=$(echo $window | cut -f9 -d "/" | cut -f4 -d ".")
         echo $win_num
         grep 'LK' ${ldhat_output_par}/${scaffold}.${win_num}.${mcmc} | \
-        awk 'BEGIN{print "iterations""\t""likelihood""\t""blocks""\t""map_length"} {print NR"\t"$5"\t"$10"\t"$15}' > ${ldhat_output_par}/${scaffold}.${win_num}.${mcmc}.chainreport.txt
+        awk 'BEGIN{print "iterations""\t""likelihood""\t""blocks""\t""map_length"} {print NR"\t"$5"\t"$10"\t"$15}' \
+        > ${mcmc_par}/${scaffold}.${win_num}.${mcmc}.chainreport.txt
         done
     done
 done
@@ -73,7 +102,8 @@ do
         win_num=$(echo $window | cut -f9 -d "/" | cut -f4 -d ".")
         echo $win_num
         grep 'LK' ${ldhat_output_nonpar}/${scaffold}.${win_num}.${mcmc} | \
-        awk 'BEGIN{print "iterations""\t""likelihood""\t""blocks""\t""map_length"} {print NR"\t"$5"\t"$10"\t"$15}' > ${ldhat_output_nonpar}/${scaffold}.${win_num}.${mcmc}.chainreport.txt
+        awk 'BEGIN{print "iterations""\t""likelihood""\t""blocks""\t""map_length"} {print NR"\t"$5"\t"$10"\t"$15}' \
+        > ${mcmc_nonpar}/${scaffold}.${win_num}.${mcmc}.chainreport.txt
         done
     done
 done
