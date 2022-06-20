26dcba3c1f6f125f0741bc15bc2bfcdcfc83cd4c
Homa Papoli Yazdi <hpapoli@gmail.com>
2022-06-20 11:47:43 +0200

    bash script for ldhat stat

---
+++ code/analysis/rho/ldhat_stat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+
+#SBATCH -A snic2022-22-149 
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 01:00:00
+#SBATCH --mail-user=homa.papoli_yazdi@biol.lu.se
+#SBATCH --mail-type=FAIL
+
+rates=$1
+locs=$2
+out_prefix=$4
+
+./LDhat/stat -input $rates -burn 1000 -loc $locs -prefix $out_prefix
