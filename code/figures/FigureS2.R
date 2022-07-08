31b22a8edc1ceca53ddc5e54f88b4d2c530e5978
Homa Papoli Yazdi <hpapoli@gmail.com>
2022-07-08 11:18:45 +0200

    figure S2

---
+++ code/figures/FigureS2.R
@@ -0,0 +1,21 @@
+#!/usr/bin/R
+
+par_frq <- read.table("../../data/vcf/par_vcf/stats_finalFilter/par.frq", skip = 1)
+nonpar_frq <- read.table("../../data/vcf/nonpar_vcf/stats_finalFilter/nonpar.frq", skip = 1)
+a_frq <- read.table("../../data/vcf/a_vcf/stats_finalFilter/a.frq", skip = 1)
+
+par_ldepth <- read.table("../../data/vcf/par_vcf/stats_finalFilter/par.ldepth.mean", skip = 1)
+nonpar_ldepth <- read.table("../../data/vcf/nonpar_vcf/stats_finalFilter/nonpar.ldepth.mean", skip = 1)
+a_ldepth <- read.table("../../data/vcf/a_vcf/stats_finalFilter/a.ldepth.mean", skip = 1)
+
+pdf("../../figures/FigureS2.pdf", w = 9, h = 6.5)
+par(mfrow=c(2,3))
+hist(par_frq$V6, xlab = "PAR alternatve allele freq.", main = NULL)
+hist(a_frq$V6, xlab = "Autosome alternatve allele freq.", main = NULL)
+hist(nonpar_frq$V6, xlab = "nonPAR alternatve allele freq.", main = NULL)
+
+hist(par_ldepth$V3, xlab = "PAR depth per site", main = NULL)
+hist(a_ldepth$V3, xlab = "Autosome depth per site", main = NULL)
+hist(nonpar_ldepth$V3, xlab = "nonPAR depth per site", main = NULL)
+
+dev.off()
--- code/figures/figures.sh
+++ code/figures/figures.sh
@@ -3,3 +3,6 @@
 mkdir -p ../../figures
 
 Rscript FigureS1.R
+
+Rscript FigureS2.R 
+
