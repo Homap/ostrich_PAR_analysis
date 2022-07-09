c30db1fa834f86b35711b3dd654ef0e916def354
Homa Papoli Yazdi <hpapoli@gmail.com>
2022-07-09 23:42:39 +0200

    edits to directory

--- code/analysis/geneticmap/get_recombination_frequency.R
+++ code/analysis/geneticmap/get_recombination_frequency.R
@@ -4,32 +4,13 @@ source("../../../software_module/R/recombination_rate.R")
 
 sex_averaged_map <- read.table("../../../data/geneticmap/sex_averaged_map_loess.txt", header = T)
 
-female_rate <- rate_function(genetic_map = sex_averaged_map, len_markers = 193, column = 5, span = 0.3)
-male_rate <- rate_function(genetic_map = sex_averaged_map, len_markers = 193, column = 6, span = 0.3)
-sex_averaged_rate <- rate_function(genetic_map = sex_averaged_map, len_markers = 193, column = 7, span = 0.3)
-
-kosambi_female <- female_rate[[1]]
-kosambi_male <-  male_rate[[1]]
-kosambi_sex_averaged <- sex_averaged_rate[[1]]
+kosambi_female <- rate_function(genetic_map = sex_averaged_map, len_markers = 193, column = 5, span = 0.3)
+kosambi_male <- rate_function(genetic_map = sex_averaged_map, len_markers = 193, column = 6, span = 0.3)
+kosambi_sex_averaged <- rate_function(genetic_map = sex_averaged_map, len_markers = 193, column = 7, span = 0.3)
 
 write.table(kosambi_female, "../../../data/geneticmap/kosambi_female.txt", quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(kosambi_male, "../../../data/geneticmap/kosambi_male.txt", quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(kosambi_sex_averaged, "../../../data/geneticmap/kosambi_sex_averaged.txt", quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
-
-start_pos <- female_rate[[2]]$start
-end_pos <- female_rate[[2]]$end
-female_pair_cm_per_mb <- round(female_rate[[2]]$pair_cm_per_site, 3)
-male_pair_cm_per_mb <- round(male_rate[[2]]$pair_cm_per_site, 3)
-sex_averaged_pair_cm_per_mb <- round(sex_averaged_rate[[2]]$pair_cm_per_site, 3)
-
-female_smoothed <- round(female_rate[[3]], 3)
-male_smoothed <- round(male_rate[[3]], 3)
-sex_averaged_smoothed <- round(sex_averaged_rate[[3]], 3)
-
-recombination_rate <- data.frame(start_pos, end_pos, female_pair_cm_per_mb, male_pair_cm_per_mb, sex_averaged_pair_cm_per_mb, 
-                                female_smoothed, male_smoothed, sex_averaged_smoothed)
-
-write.table(recombination_rate, "../../../data/geneticmap/recombination_rate.txt", quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
 
 ## Genetic length in PAR in females, males and sex-averaged where PAR is defined at 53065777
 female_PAR_length = 80.628  
