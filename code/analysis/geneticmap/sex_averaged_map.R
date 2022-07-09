c8c3617c3b429e299394c3b789ff16565ad2aa42
Homa Papoli Yazdi <hpapoli@gmail.com>
2022-07-09 22:10:26 +0200

    updated genetic map coordinates

--- code/analysis/geneticmap/sex_averaged_map.R
+++ code/analysis/geneticmap/sex_averaged_map.R
@@ -7,22 +7,31 @@
 library(dplyr)
 
 ## Genetic map
-female_map <- read.table("../../../data/geneticmap/LGZ3.female.lifted.cleaned.bed.txt", sep = "")
-colnames(female_map) <- c("Chr", "Pos", "PosPlus1", "mapname", "cM", "scaffold")
-male_map <- read.table("../../../data/geneticmap/LGZ3.male.lifted.cleaned.bed.txt", sep = "")
-colnames(male_map) <- c("Chr", "Pos", "PosPlus1", "mapname", "cM", "scaffold")
+female_map <- read.table("../../../data/geneticmap/LGZ3.female.cleaned.chr.bed", sep = "")
+colnames(female_map) <- c("Chr", "chrPos", "chrPosPlus1", "scaffold", "scaffoldpos", "scaffoldposplus1", "cM")
+male_map <- read.table("../../../data/geneticmap/LGZ3.male.cleaned.chr.bed", sep = "")
+colnames(male_map) <- c("Chr", "chrPos", "chrPosPlus1", "scaffold", "scaffoldpos", "scaffoldposplus1", "cM")
 
-both_sex_map <- inner_join(female_map, male_map, by = "Pos")
+both_sex_map <- inner_join(female_map, male_map, by = "chrPosPlus1")
 
-par <- both_sex[which(both_sex$Pos < 53065777),]
-nonpar <- both_sex[which(both_sex$Pos >= 53065777),]
+start <- both_sex_map$chrPosPlus1[1:192]
+end <- both_sex_map$chrPosPlus1[2:193]
+female_cM <- round(abs(both_sex_map$cM.x[2:193] - both_sex_map$cM.x[1:192]), 3)
+male_cM <- round(abs(both_sex_map$cM.y[2:193] - both_sex_map$cM.y[1:192]), 3)
+Pos <- (start + end)/2
+first_scaffold <- both_sex_map$scaffold.x[1:192]
+second_scaffold <- both_sex_map$scaffold.x[2:193]
+scaffold_start <- both_sex_map$scaffoldposplus1.x[1:192]
+scaffold_end <- both_sex_map$scaffoldposplus1.x[2:193]
 
-sex_averaged_cM <- c((par$cM.x + par$cM.y)*0.5, (nonpar$cM.y)*(2/3))
-Pos <- both_sex$Pos
-female_cM <- both_sex$cM.x
-male_cM <- both_sex$cM.y
+rate_data <- data.frame(start, end, Pos, female_cM, male_cM, first_scaffold, second_scaffold, scaffold_start, scaffold_end)
 
-sex_averaged_map <- data.frame(Pos, female_cM, male_cM,sex_averaged_cM)
+par <- rate_data[which(rate_data$start < 53050777),]
+nonpar <- rate_data[which(rate_data$start >= 53050777),]
+
+sex_averaged_cM <- c((par$female_cM + par$male_cM)*0.5, (nonpar$male_cM)*(2/3))
+
+sex_averaged_map <- data.frame(start, end, Pos, female_cM, male_cM, sex_averaged_cM, first_scaffold, second_scaffold, scaffold_start, scaffold_end)
 
 # LOESS for smoothing of the three maps
 female_map_loessMod25 <- loess(female_cM ~ Pos, data=sex_averaged_map, span=0.25) # 25% smoothing span
@@ -37,7 +46,10 @@ female_smoothed25 <- round(female_map_smoothed25, 3)
 male_smoothed25 <- round(male_map_smoothed25, 3)
 sex_averaged_smoothed25 <- round(sex_average_map_smoothed25, 3)
 
-sex_averaged_map_loess <- data.frame(Pos, female_cM, male_cM,sex_averaged_cM, 
+chrom = rep("ChrZ", 192)
+
+sex_averaged_map_loess <- data.frame(chrom, start, end, Pos, female_cM, male_cM,sex_averaged_cM,
+                                    first_scaffold, second_scaffold, scaffold_start, scaffold_end,
                                     female_smoothed25, male_smoothed25, sex_averaged_smoothed25)
 
 write.table(sex_averaged_map_loess, "../../../data/geneticmap/sex_averaged_map_loess.txt", quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
