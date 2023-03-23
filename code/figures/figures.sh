9fb7af31fbaeac681df22441d155cc94281f79d5
Homa Papoli Yazdi <hpapoli@gmail.com>
2023-03-23 19:27:09 +0100

    edits to run script for figures

--- code/figures/figures.sh
+++ code/figures/figures.sh
@@ -6,6 +6,8 @@ Rscript FigureS1.R
 
 Rscript FigureS2.R 
 
+Rscript FigureS3.R
+
 Rscript Figure2.R
 
 Rscript Figure3.R
@@ -14,3 +16,4 @@ Rscript Figure4.R
 
 # intro_figure.R contains the recombination frequency plot and the plot showing genes along the chromosome. 
 # To produce the final figure, after plotting each in R, I assembled them in inkscape.
+# The LD matrix in Figure S4 was produced by th origram [LDBlockShow](https://github.com/BGI-shenzhen/LDBlockShow)
